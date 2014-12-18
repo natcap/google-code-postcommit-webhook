2fc468200a2e86c761158bd192a5ef654b077ace
Richard Sharp <richpsharp@gmail.com>
2014-12-18 11:13:07 -0800

    cleaning up for pylint and general file structure

--- google_code_hook.py
+++ google_code_hook.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python
+"""Module used to process a post commit hook from Google code to
+    automatically update a webpage that exists in that repoistory."""
+
 import cgitb
 import subprocess
 import json
@@ -6,47 +9,36 @@ import sys
 import os
 import time
 
-cgitb.enable(display=0, logdir="/usr/local/natcap-webserver-data/post_commit_hook/cgi-bin/google-code-postcommit-webhook")
+cgitb.enable(display=0, logdir=".")
 
 print "Content-Type: text/html" # HTML is following
 print # blank line, end of headers
 
-data = sys.stdin.read()
 
-repository_dir = os.path.expanduser("~/invest-natcap.webpage/")
-with open('post_commit_hooks.log', 'a') as f:
+REPOSITORY_PATH = "https://code.google.com/p/invest-natcap.webpage/"
+REPOSITORY_DIR = os.path.expanduser("~/invest-natcap.webpage/")
+with open('post_commit_hooks.log', 'a') as LOG_FILE:
+    NULL_FH = open("NUL", "w")
     try:
-        f.write(data + '\n')
-        f.flush()
-        hook_data = json.loads(data)
-        f.write(str(hook_data) + '\n')
-        f.flush()
-        f.write(hook_data["repository_path"] + '\n')
-        f.flush()
-        if hook_data["repository_path"] == "https://code.google.com/p/invest-natcap.webpage/":
-            if not os.path.isdir(repository_dir):
-                f.write("cloning\n")
-                f.flush()
+        HOOK_DATA = json.loads(sys.stdin.read())
+        if HOOK_DATA["repository_path"] == REPOSITORY_PATH:
+            if not os.path.isdir(REPOSITORY_DIR):
                 subprocess.call(
-                    ["hg", "clone", hook_data["repository_path"], repository_dir])
+                    ["hg", "clone", HOOK_DATA["repository_path"], REPOSITORY_DIR],
+                    stdout=NULL_FH, stderr=NULL_FH)
             else:
-                f.write("pulling\n")
-                f.flush()
                 subprocess.call(
-                    ["hg", "pull", "--repository", repository_dir])
-            f.write("updating\n")
-            f.flush()
+                    ["hg", "pull", "--repository", REPOSITORY_DIR],
+                    stdout=NULL_FH, stderr=NULL_FH)
             subprocess.call(
-                ["hg", "up", "-C",  "--repository", repository_dir])
-
-            f.write("syncing\n")
-            f.flush()
+                ["hg", "up", "-C", "--repository", REPOSITORY_DIR],
+                stdout=NULL_FH, stderr=NULL_FH)
             subprocess.call(
-                [os.path.join(repository_dir, "sync.sh")])
-            f.write("done\n%s\n" % time.strftime("%c"))
+                [os.path.join(REPOSITORY_DIR, "sync.sh")])
+            LOG_FILE.write("[%s] successfuly updated\n" % time.strftime("%c"))
 
-    except Exception as e:
-        f.write("Exception %s\n" % (str(e)))
-        f.flush()
-
-print 'yo'
+    except Exception as exception:
+        LOG_FILE.write(
+            "[%s] Exception\n%s\n" % (time.strftime("%c"), str(exception)))
+        LOG_FILE.flush()
+    NULL_FH.close()
